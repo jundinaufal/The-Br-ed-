4f7fc0f9293298aac36e027e6e9170a074becbed
jundinaufal <naufal6523@gmail.com>
2024-07-07 17:21:20 +0700

    feat: some modify

--- public/src/Views/Home.jsx
+++ public/src/Views/Home.jsx
@@ -9,31 +9,29 @@ function Home({ url }) {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState('');
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const [totalPages, setTotalPages] = useState(0);
-    // const [limit, setLimit] = useState(6); // number of products per page
-    // const [categories, setCategory] = useState(''); // add a state to store the selected category
-
-    // setCategory(products.Category)
-
-    console.log(products, `category di home`);
+    const [category, setCategory] = useState('ShowAll'); // initialize category state with 'ShowAll'
+    const [allProducts, setAllProducts] = useState([]); // store all products for filtering
+    const [sort, setSort] = useState('nameASC'); // initialize sort state with 'nameASC'
+    const [currentPage, setCurrentPage] = useState(1); // initialize current page state
+    const [productsPerPage, setProductsPerPage] = useState(9); // initialize products per page state
+    const categories = [
+        { key: 1, value: 'Ladies' },
+        { key: 2, value: 'Men' },
+        { key: 3, value: 'Kids' },
+    ];
 
     async function fetchProducts() {
         try {
             setLoading(true);
-            const { data } = await axios.get(`${url}/public/products`, {
-                // params: {
-                //     page: currentPage,
-                //     limit: limit,
-                //     category: products.Category, // add category to the params
-                // },
-            });
+            const { data } = await axios.get(`${url}/public/products`);
 
+            console.log(data, `data di home`);
             setProducts(data.data);
-            // setTotalPages(Math.ceil(data.total / limit));
+            setAllProducts(data.data); // store all products for filtering
+
         } catch (error) {
             Toastify({
-                text: error.response,
+                text: error.response.data.error,
                 duration: 2000,
                 newWindow: true,
                 close: true,
@@ -54,34 +52,72 @@ function Home({ url }) {
 
     useEffect(() => {
         fetchProducts()
-    }, [search])
+    }, [])
 
     // search
     function searchOnChange(event) {
         let newSearch = event.target.value;
         setSearch(newSearch);
+        filterProducts(newSearch, category);
     }
 
     function handleCategoryChange(event) {
         let newCategory = event.target.value;
         setCategory(newCategory);
+        filterProducts(search, newCategory);
     }
 
-    // const handlePageChange = (page) => {
-    //     setCurrentPage(page);
-    // };
+    function handleSortChange(event) {
+        let newSort = event.target.value;
+        setSort(newSort);
+        sortProducts(newSort, products);
+    }
 
-    // const handlePrevious = () => {
-    //     if (currentPage > 1) {
-    //         setCurrentPage(currentPage - 1);
-    //     }
-    // };
+    // filter products based on search and category
+    function filterProducts(searchQuery, category) {
+        let filteredProducts = allProducts;
 
-    // const handleNext = () => {
-    //     if (currentPage < totalPages) {
-    //         setCurrentPage(currentPage + 1);
-    //     }
-    // };
+        if (searchQuery!== '') {
+            filteredProducts = filteredProducts.filter((product) => {
+                const productName = product.name.toLowerCase();
+                return productName.includes(searchQuery.toLowerCase());
+            });
+        }
+
+        if (category!== 'ShowAll') {
+            filteredProducts = filteredProducts.filter((product) => {
+                return product.categoryId === parseInt(category);
+            });
+        }
+
+        setProducts(filteredProducts);
+    }
+
+    // sort products based on sort option
+    function sortProducts(sortOption, products) {
+        let sortedProducts = products;
+
+        if (sortOption === 'nameASC') {
+            sortedProducts = products.sort((a, b) => {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            });
+        } else if (sortOption === 'priceDESC') {
+            sortedProducts = products.sort((a, b) => b.price - a.price);
+        }
+
+        setProducts(sortedProducts);
+    }
+
+    // pagination
+    function paginate(products) {
+        const indexOfLastProduct = currentPage * productsPerPage;
+        const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+        const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+        return currentProducts;
+    }
 
     return (
         <>
@@ -96,25 +132,30 @@ function Home({ url }) {
                             onChange={searchOnChange}
                         />
                     </form>
-                    <select className="select select-bordered" onChange={handleCategoryChange}>
-                        <option disabled selected>Filter</option>
-                        {products.map((product) => (
-                            <option key={product.Category.id}> {product.Category.name}
+                    <select className="select select-bordered" onChange={handleCategoryChange} value={category}>
+                        <option value="ShowAll">Show All</option>
+                        {categories.map((category) => (
+                            <option key={category.key} value={category.key}>
+                                {category.value}
                             </option>
                         ))}
                     </select>
-                    {/* <button className="btn">Search</button> */}
+                    <select className="select select-bordered" onChange={handleSortChange} value={sort}>
+                        <option value="nameASC">Name (A-Z)</option>
+<option value="priceDESC">Price (High-Low)</option>
+                        {/* add more sort options here */}
+                    </select>
                 </div>
             </div>
 
-            {loading ? (
+            {loading? (
                 <div className="mt-32 flex justify-center items-center">
                     <img src={gearLoad} />
                 </div>
             ) : (
                 <div id="PAGE-HOME" className="p-3">
                     <div className="grid grid-cols-3 gap-10 px-5 my-8 bg-white flex-wrap">
-                        {products.map((product) => {
+                        {paginate(products).map((product) => {
                             return (
                                 <Link key={product.id} to={`/public/products/${product.id}`}>
                                     <Card product={product} />
@@ -122,32 +163,10 @@ function Home({ url }) {
                             );
                         })}
                     </div>
-
-                    {/* <div className="join">
-                        <button
-                            className="join-item btn"
-                            onClick={handlePrevious}
-                            disabled={currentPage === 1}
-                        >
-                            «
-                        </button>
-                        {Array.from({ length: totalPages }, (_, i) => (
-                            <button
-                                key={i + 1}
-                                className={`join-item btn ${currentPage === i + 1? 'bg-blue-500' : ''}`}
-                                onClick={() => handlePageChange(i + 1)}
-                            >
-                                {i + 1}
-                            </button>
-                        ))}
-                        <button
-                            className="join-item btn"
-                            onClick={handleNext}
-                            disabled={currentPage === totalPages}
-                        >
-                            »
-                        </button>
-                    </div> */}
+                    <div className="flex justify-center mt-5">
+                        <button className="btn btn-neutral" onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                        <button className="btn btn-grey" onClick={() => setCurrentPage(currentPage + 1)} disabled={paginate(products).length < productsPerPage}>Next</button>
+                    </div>
                 </div>
             )}
         </>
